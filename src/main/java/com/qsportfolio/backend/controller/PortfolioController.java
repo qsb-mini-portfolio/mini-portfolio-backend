207f169ea8220f5f2229885051a44c94b5dabae0
Quentin WESSER <qwesser@consultants-solutec.fr>
2025-09-18 13:44:29 +0200

    Remove unused import

--- src/main/java/com/qsportfolio/backend/controller/PortfolioController.java
+++ src/main/java/com/qsportfolio/backend/controller/PortfolioController.java
@@ -5,7 +5,6 @@ import com.qsportfolio.backend.response.portfolio.PortfolioResponseFactory;
 import com.qsportfolio.backend.service.PortfolioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
